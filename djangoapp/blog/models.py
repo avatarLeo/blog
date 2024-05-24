de67e8a6269e4dd0dccc7fdff11ed8217b2fc94e
avatarLeo <leandroalmeida055@gmail.com>
2024-05-24 20:32:17 -0300

    Aula 538 criando o model post

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from utils.rands import slugify_new
+from django.contrib.auth.models import User
 
 class Tag(models.Model):
     class Meta:
@@ -60,4 +61,54 @@ class Page(models.Model):
     def __str__(self) -> str:
         return self.title
 
+
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(
+        unique=True, default="",
+        null=False, blank=True, max_length=255
+    )
+    excerpt = models.CharField(max_length=150)
+
+    is_published = models.BooleanField(
+        default=False,
+        help_text= 'Esse campo precisará estar marcado para a post ser exibida publicamente'
+        )
+    content = models.TextField()
+    cover = models.ImageField(upload_to='posts/%Y/%m/', blank=True, default='')
+    cover_in_post_content = models.BooleanField(
+        default=True,
+        help_text='Exibir a imagem de capa também dentro do conteúdo do post?'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, blank=True,
+        null=True, related_name='post_created_by'
+    )
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='post_updated_by'
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True,
+        blank=True, default=None
+    )
+    tags = models.ManyToManyField(Tag, blank=True, default='')
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.title
 

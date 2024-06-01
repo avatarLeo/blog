431ce2304c17bd4324ee30c474937f89b1b721c1
avatarLeo <leandroalmeida055@gmail.com>
2024-06-01 19:33:41 -0300

    Aula 542 filtrando e ordenando os posts

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
+from blog.models import Post
+from django.core.paginator import Paginator
 
-# Create your views here.
+PER_PAGE = 9
 
 def index(request):
+    posts = Post.objects.filter(is_published=True).order_by('-pk')
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     return render(
-       request, 'blog/pages/index.html'
+       request, 'blog/pages/index.html',
+       {
+           'page_obj': page_obj,
+       }
     )
 
 

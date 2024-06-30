629b37a6f2a972e6e790d22b441ee42840c8db56
avatarLeo <leandroalmeida055@gmail.com>
2024-06-30 11:22:58 -0300

    Alteração no wsgi

--- djangoapp/project/wsgi.py
+++ djangoapp/project/wsgi.py
@@ -12,10 +12,12 @@ from dotenv import load_dotenv
 from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
+#dotenv
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / 'dotenv_files' / '.env', override=True)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
 
-#dotenv
-BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / 'dotenv_files' / '.env', override=True)
+
